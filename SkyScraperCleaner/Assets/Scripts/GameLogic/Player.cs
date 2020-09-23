30944e10d883e332a428c100f6bbc34350d73286
Omri Zuta <omrizu@mta.ac.il>
2020-09-23 16:42:24 +0300

    sort folders added weapons to scene

--- SkyScraperCleaner/Assets/Scripts/GameLogic/Player.cs
+++ SkyScraperCleaner/Assets/Scripts/GameLogic/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private GameObject m_LookAtMeWindow; 
-    
+    private GameObject m_LookAtMeWindow;
+
     private Transform m_selection;
     // Start is called before the first frame update
     void Start()
@@ -26,7 +26,7 @@ public class Player : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
         if (Physics.Raycast(ray, out hit))
         {
-            
+
             if (hit.rigidbody != null)
             {
                 var selection = hit.transform;
@@ -34,14 +34,21 @@ public class Player : MonoBehaviour
                 Debug.Log(selection.tag);
                 if (selection.CompareTag("GameBuilding"))
                 {
-                    if(Input.anyKeyDown && this.transform.position.y <43)
+                    if (Input.anyKeyDown && this.transform.position.y < 43)
                     {
                         Debug.Log("Moving up!!!");
                         Vector3 movementChange = new Vector3();
-                        movementChange += (hit.transform.position- transform.position);
+                        movementChange += (hit.transform.position - transform.position);
                         movementChange.z = 0;
                         movementChange *= 0.05f;
                         this.transform.position -= movementChange;
+                    }
+                }
+                else if (selection.CompareTag("DirtyWindow"))
+                {
+                    if (Input.anyKeyDown)
+                    {
+
                     }
                 }
             }
@@ -50,7 +57,7 @@ public class Player : MonoBehaviour
     private void resetPosition()
     {
         transform.position = new Vector3(-28, 5, -12);
-        Vector3 lookAt =new Vector3();
+        Vector3 lookAt = new Vector3();
         lookAt = m_LookAtMeWindow.transform.position;
         lookAt.x -= 0.3f;
         transform.LookAt(lookAt);
