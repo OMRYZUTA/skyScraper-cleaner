16b4b4cdcf2a9f9f4a3237730d3c59adf3609930
Omri Zuta <omrizu@mta.ac.il>
2020-09-27 16:26:22 +0300

    started UI

---
+++ SkyScraperCleaner/Assets/Scripts/UI/PanelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PanelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject m_AreYouSurePopUp;
+    [SerializeField] GameObject m_HowToPlayPopUp;
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void PopUpHowToPlay()
+    {
+        m_HowToPlayPopUp.SetActive(true);
+    }
+    public void Exit()
+    {
+        m_AreYouSurePopUp.SetActive(true);
+    }
+}
---
+++ SkyScraperCleaner/Assets/Scripts/UI/PopUpManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject m_AreYouSurePopUp;
+    [SerializeField] GameObject m_HowToPlayPopUp;
+    public void PlayerWantsToExit()
+    {
+        m_AreYouSurePopUp.SetActive(true);
+    }
+    public void ShowInstructions()
+    {
+        m_HowToPlayPopUp.SetActive(true);
+    }
+    public void PlayerSureAboutExit()
+    {
+        Application.Quit();
+    }
+    public void PlayerWantsToStay()
+    {
+        m_AreYouSurePopUp.SetActive(false);
+    }
+    public void CloseHowToPlayPopUp()
+    {
+        m_HowToPlayPopUp.SetActive(false);
+    }
+}
