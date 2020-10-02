cd174fd7b09bf1e66def330a08f3af9a57f1292f
Omri Zuta <omrizu@mta.ac.il>
2020-10-02 14:17:49 +0300

    highScoreTableStarted

---
+++ SkyScraperCleaner/Assets/Scripts/UI/HighScoreTable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("EntryContainer");
+        entryTemplate = entryContainer.Find("HighScoreEntryTemplate");
+        entryTemplate.gameObject.SetActive(false);
+        float templateHeight = 0.02f;
+        for(int i = 0; i < 6; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition =new Vector2(0,-templateHeight*i);
+            entryTransform.gameObject.SetActive(true);
+
+        }
+    }
+
+}
